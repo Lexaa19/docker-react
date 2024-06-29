691b6c960319bd19ec494ccf283b3d4ceb06c81c
Lexaa19 <caliopi.durdunescu@yahoo.com>
2024-06-29 10:20:04 +0300

    Update code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders hello link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/hello! how are you?/i);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("renders learn react link", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
